85ef35944108b19ee09bc856ac5321cca816c782
Alejandro Gonzalez <alex1123.nel@gmail.com>
2025-09-17 20:17:46 -0600

    1-Creacion de una base de datos en MSSQL

---
+++ 1-Creacion y administracion de una base de datos/1-Creacion de una base de datos en MSSQL.sql
@@ -0,0 +1,10 @@
+-- ***** Creacion de la base de datos *****
+CREATE DATABASE AzulSchoolMSSQLAdministration;
+GO -- Indica que debe ejecutar el bloque de consultas (util cuando se trabaja en CLI)
+
+USE AzulSchoolMSSQLAdministration; -- Indicamos que usaremos la base de datos que acabamos de crear
+
+-- ***** Eliminacion de una base de datos *****
+CREATE DATABASE test; -- Se crea la base de datos
+GO
+DROP DATABASE test; -- Se elimina la base de datos
