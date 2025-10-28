4015ee4f752ecb8ce3f63a916e669433b8a36f68
Alejandro Gonzalez <alex1123.nel@gmail.com>
2025-10-27 20:54:15 -0600

    Eventos extendidos

---
+++ 6-Monitoreo y auditorias/4-Eventos extendidos.sql
@@ -0,0 +1,18 @@
+/*
+Con los pasos que vienen en las Notas del modulo, cree una sesion que registra cuando se crea/elimina una base de datos
+*/
+-- Creamos varias bases de datos para ver los registros en la sesion de eventos extendidos que creamos
+CREATE DATABASE TEST;
+GO
+CREATE DATABASE TEST2;
+GO
+CREATE DATABASE TEST3;
+GO
+
+-- Eliminamos varias bases de datos para ver los registros en la sesion de eventos extendidos que creamos
+DROP DATABASE TEST;
+GO
+DROP DATABASE TEST2;
+GO
+DROP DATABASE TEST3;
+GO
